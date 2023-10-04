55a69f48417d7892924acb5372eb1cdf887bf72f
Manel Soler <manelsoler@gmail.com>
2023-10-04 14:43:54 +0200

    create post and user model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_lenght=255)
+  content= models.TextField()
+  published_date = models.DateTimeField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+     return self.title
+
+class Author(models.Model):
+   author_name = models.TextField(max_length= (80))
