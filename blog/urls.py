9b93da291667f2c2e86464919bb7854df487b2bf
Manel Soler <manelsoler@gmail.com>
2023-10-11 20:04:05 +0200

    Feat read_post by id end point

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.post_list, name='post_list'),
   path('create/', views.create_post, name='create_post'),
+  path('post/<int:post_id>/', views.read_post, name='read_post'),
+
 ]
